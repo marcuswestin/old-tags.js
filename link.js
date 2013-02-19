37ef70247cd42e5f3065b7a045ea4d35348ea4b7
Marcus Westin <marcus.westin@gmail.com>
2013-02-19 09:24:53 -0800

    implement link.js - `link("className", "Google", "http://google.com")` or `link("className", "Tap Me", function() { ... })`

---
+++ link.js
@@ -0,0 +1,21 @@
+var tags = require('./tags')
+var button = require('./button')
+
+var div = tags('div')
+var a = tags('a')
+
+module.exports = link
+
+link.defaultTarget = null
+function link(className, title, path) {
+	if (arguments.length == 2) {
+		path = title
+		title = className
+		className = ''
+	}
+	if (typeof path == 'function') {
+		return div('link '+className, title, button(path))
+	} else {
+		return a('link '+className, title, { href:path, target:link.defaultTarget })
+	}
+}
